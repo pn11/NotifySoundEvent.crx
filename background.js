0b342450f871d3070f6a4a3f899d2bf2da01f570
OKA Naoya <pn11@users.noreply.github.com>
2017-12-06 22:28:02 +0900

    make notification clickable

--- background.js
+++ background.js
@@ -1,13 +1,28 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	var windowId = tab.windowId;
 	if (changeInfo.audible) {
 		var opt = {
 			type: "basic",
 			title: tab.title,
 			message: "The tab has sound.",
 			//		iconUrl: tab.favIconUrl // doesn't work
-			iconUrl: "icon.png"
+			iconUrl: "icon.png",
+			isClickable: true
 		};
 
-		chrome.notifications.create("999", opt, function (notificationId) { });
+		chrome.notifications.create("999", opt, function (notificationId) {
+			chrome.notifications.onClicked.addListener(function (notificationId) {
+				var updateInfo = {
+					focused: true
+				};
+				chrome.windows.update(windowId, updateInfo, function (window) {
+					var object = {
+						active: true
+					};
+					chrome.tabs.update(tabId, object, function (tab) { });
+				});
+				chrome.notifications.clear("999", function (wasCleared) { });
+			});
+		});
 	}
 });
